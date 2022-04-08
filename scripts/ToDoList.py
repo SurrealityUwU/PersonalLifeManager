e578b4d0ea0cb564ad2a51504b3cf2687d7f798c
SurrealityUwU <chaowanonsrisilpa@gmail.com>
2022-04-08 16:03:59 +0700

    ToDoList in progress

--- scripts/ToDoList.py
+++ scripts/ToDoList.py
@@ -1,5 +1,25 @@
 
 class ToDoList:
-    pass
+    remove_after_days = 3
+    log_remove_number_days = 30
 
+    def __init__(self):
+        self.tasks = []
+        self.properties = []
+        self.filters = []
+        self.groups = []
+        self.history_log = []
+        self.highlight_of_day
+        self.optimal_day_off
 
+    def add_filter(self):
+        # TODO
+        pass
+
+    def conseqeunces_day_off(self):
+        # TODO
+        pass
+
+    def create_property(self, String):
+        # TODO
+        pass
---
+++ scripts/testMain.py
@@ -0,0 +1,5 @@
+import TaskManager
+
+if __name__ == "__main__":
+    tm = TaskManager.TaskManager()
+    
